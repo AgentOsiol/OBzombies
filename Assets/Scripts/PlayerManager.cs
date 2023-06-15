763336c15982db7d461f88d583c548e3fda59261
AgentOsiol <113114202+AgentOsiol@users.noreply.github.com>
2023-06-15 14:46:42 +0100

    navmesh testing

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,21 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
-public class AIscript : MonoBehaviour
+
+public class PlayerManager : MonoBehaviour
 {
-    public NavMeshAgent Enemy;
-
-    public Transform Player;
-    public Transform Base;
-
-    public string BaseExists;
-
-    
     // Start is called before the first frame update
     void Start()
     {
-        Enemy = GetComponent<NavMeshAgent>();
+        
     }
 
     // Update is called once per frame
